238c0bd1498d62309fad87ca76fcd87497157183
aripineninavya-rgb <aripineninavya@gmail.com>
2025-11-20 16:00:04 +0100

    added all projects into my portfolio

--- React-project-portfolio/src/App.jsx
+++ React-project-portfolio/src/App.jsx
@@ -1,21 +1,36 @@
 import Project from "./Components/Project.jsx";
 import booktracker from "./assets/booktracker.png";
 import fromsimg from "./assets/forms.png";
-import sasimg from "./assets/sas.png"
+import sasimg from "./assets/sas.png";
 
 function App() {
   return(
     <div>
       <Project
-      projectnName = "Book Tracker"
+      projectName = "Form"
+      screenshot = {fromsimg}
+      tech = "HTML, CSS, Forms"
+      repo = "https://github.com/aripineninavya-rgb/form-design"
+      shortNote = "This project is a form that collects user information and displays the submitted results. I learned how to use HTML input fields, labels, and validation."
+      />
+
+      <Project
+      projectName = "Book Tracker"
       screenshot = {booktracker}
       tech = "HTML, CSS, JavaScript"
       repo = "https://github.com/aripineninavya-rgb/indroduction-to-javascript"
       shortNote = "A small app that tracks books I have read. I learned arrays, objects, and functions."
       />
+
+      <Project
+      projectName = "Book Tracker"
+      screenshot = {sasimg}
+      tech = "HTML, CSS, SAS"
+      repo = "https://github.com/aripineninavya-rgb/sasdocument"
+      shortNote = "I learned how to arrange content using HTML elements, how to style a document using CSS, and how to create proper spacing, alignment, and formatting similar to a real official document."
+      />
     </div>
     )
-
 }
 
 export default App
--- React-project-portfolio/src/Components/Project.jsx
+++ React-project-portfolio/src/Components/Project.jsx
@@ -2,14 +2,15 @@ import "./Project.css"
 
 function Project(props){
     return(
-    <div>
-        <h1>Project Name: {props.projectName}</h1>
-        <img src={props.screenshot} alt={props.name} />
-        <h3>Techs used{props.tech}</h3>
-        <a href={props.repo}>Github Link</a>
+    <div className="project">
+        <h1>Project:1</h1>
+        <h2>Project Name: {props.projectName}</h2>
+        <img className="img" src={props.screenshot} alt={props.name} />
+        <h3>Techs used: {props.tech}</h3>
+        <h4>CLICK HERE:<a href={props.repo}>Github Link</a></h4>
         <p>{props.shortNote}</p>
     </div>
-)
+    )
 }
 
 export default Project;
